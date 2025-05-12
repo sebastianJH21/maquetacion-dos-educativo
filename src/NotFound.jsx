5d5c82154d17400c34621d307544c535a3cb10df
Sebastian Jimenez <sjimnezc@cesde.net>
2025-05-11 21:07:14 -0500

    Corrección de redirección de la pagina clientes

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,16 +1,11 @@
-import { useLocation } from "react-router-dom"
 function NotFound() {
-  const location = useLocation();
-  const query = new URLSearchParams(location.search);
-  const emial = query.get('email');
   return (
     <>
-          <div className="card">
-              <h1>404</h1>
-              <h2>Page not found</h2>
-              <a href="/">Back to home</a>
-              <h1>{emial}</h1>
-        </div>
+      <div className="card">
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <a href="/">Back to home</a>
+      </div>
     </>
   )
 }
--- src/pages/clients/SearchClient.jsx
+++ src/pages/clients/SearchClient.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
+
 import './SearchClient.css'
 import { alertMsg } from '../../helpers/functions'
 import { useNavigate } from 'react-router-dom'
-import { clients } from '../../services/dataBase'
 
 function SearchClient() {
   let navigate = useNavigate();
   function search(){
 
-    console.log(clients)
     let email = document.querySelector('#emailClient').value
     if (email) {
       const regex = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
-      regex.test(email);
-      if (regex.test(email)) {
+      if (!regex.test(email)) {
         alertMsg('Error', 'Incorrect email format', 'info')
-        navigate(`/client?${email}`)
+      } else {
+        navigate('/clients')
       }
     }else{
       alertMsg('Error', 'Please enter a client email', 'error')
@@ -23,15 +21,14 @@ function SearchClient() {
     }
   }
   return (
-    <>
+    <main>
       <h1>Search Cliente</h1>
       <section>
         <input type="email" name='emailClient' id='emailClient' placeholder="Email" />
-        <button onClick={search}>
-          Search
-        </button>
+        <button onClick={search}>Search</button>
+        <a href="/search-products">Search Product</a>
       </section>
-    </>
+    </main>
   )
 }
 
