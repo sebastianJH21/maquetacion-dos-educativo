4952916a1bf4046ae00fb0eceb88b17801781da4
sebastianJH21 <sjimnezc@cesde.net>
2025-05-13 13:33:16 -0500

    Se agregan las imagenes para los productos

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -3,12 +3,12 @@ import './Products.css';
 function Products() {
     return (
         <section className="products">
-            <Card props={{ img: './public/background.png', page: '/product' }} />
-            <Card props={{ img: './public/background.png', page: '/product' }} />
-            <Card props={{ img: './public/background.png', page: '/product' }} />
-            <Card props={{ img: './public/background.png', page: '/product' }} />
-            <Card props={{ img: './public/background.png', page: '/product' }} />
-            <Card props={{ img: './public/background.png', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productOne.jpg', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productTwo.jpg', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productThree.jpeg', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productFour.avif', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productFive.webp', page: '/product' }} />
+            <Card props={{ img: './public/img/products/productSix.png', page: '/product' }} />
             <a href="/search-products">Home</a>
         </section>
     )
